9746c03dae2bc8cf1aeeac486777687cf4824799
eylonsade1 <eylons@post.bgu.ac.il>
2021-12-28 15:51:37 +0200

    make sure before it gets deleted

--- client.py
+++ client.py
@@ -3,10 +3,10 @@ import struct
 from threading import Thread
 from scapy.arch import get_if_addr
 
+UDP_IP = '127.0.0.1'
 UDP_PORT = 13117
 MESSAGE_LENGTH = 1024
 TIME_TO_PLAY = 10  # seconds
-UDP_IP = "172.1.255.255"
 
 """
 gets messages from the server over TCP
@@ -14,9 +14,7 @@ gets messages from the server over TCP
 
 
 def get_from_server(sock):
-    print("we are in get from")
     print(sock.recv(MESSAGE_LENGTH).decode())
-    print("finished get from")
 
 
 """
@@ -25,7 +23,7 @@ sends messages to the server over TCP
 
 
 def send_to_server(sock, inputMessage):
-    data = input(inputMessage)
+    data = input(inputMessage+"\n")
     sock.sendall(data.encode())
 
 
@@ -34,42 +32,36 @@ sets UDP socket
 """
 
 
-def setUDPSocket():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # init UDP socket
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((UDP_IP, UDP_PORT))
-    return sock
-
 
 def main():
     print("Client started, listening for offer requests...")  # waits for server suggestion
-    UDPsock = setUDPSocket()
+    UDPsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # init UDP socket
+    UDPsock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    UDPsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    UDPsock.bind((UDP_IP,UDP_PORT))
     time.sleep(0.1)
     data, address = UDPsock.recvfrom(12) # getting data andd adress from the server message
+    print(data)
     serverIP = str(address[0])
     try:
         magicCookie, message_type, server_tcp_port = struct.unpack('LBH', data)  # get message in specific format
+        print(str(server_tcp_port))
         if magicCookie == 0xabcddcba or message_type == 0x2:  # check if message is as expected
             print("Received offer from " + serverIP + ", attempting to connect...")
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # init TCP socket
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-            print("starting connect")
             sock.connect((serverIP, server_tcp_port))
-            print("serverIP --> " + serverIP)
-            print("server_tcp_port --> " + str(server_tcp_port))
             send_to_server(sock, "Enter your team's name: ")  # send team's name to server
-            print("name sent")
             get_from_server(sock)  # the game begin message
             ansThread = Thread(target=send_to_server,args=(sock,"",),daemon=True)
             ansThread.start()
             get_from_server(sock)  # the game end message
+            sock.close()
         else:
             print("Bad UDP Message Format")  # got message not in the expected format
     except Exception as e:
         print(e)
-    sock.close()
-    UDPsock.close()
+        UDPsock.close()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from scapy.all import get_if_addr
 from random import randrange
 
 ip_address = get_if_addr("eth1")
-UDP_IP = "172.1.255.255"
+UDP_IP = '127.0.0.1'
 UDP_PORT = 13117
 TCP_PORT = 2132
 MESSAGE_LENGTH = 1024
@@ -25,10 +25,10 @@ def make_bytes_message():
 
 def send_broadcast(clients):
     message = "Server started, listening on IP address " + ip_address
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP need to check ipproto
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP need to check ipproto
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # allow server to send broadcasts
-    sock.bind((UDP_IP, TCP_PORT))
+    sock.bind((UDP_IP, UDP_PORT))
     print(message)
     while len(clients) < 2:
         send_bytes = make_bytes_message()
@@ -122,18 +122,24 @@ def closeSockets(clients):
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:  # init the TCP socket
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)  # allow use 2 sockets from the same port
-        sock.bind((ip_address, TCP_PORT))  # bind the socket with our port
+        sock.bind(("", TCP_PORT))  # bind the socket with our port
         sock.listen()  # set queue of waiting size to num of connections -1
         while True:
-            clients = list()  # client list
-            client_connector = Thread(target=connect_clients, args=(clients, sock,))  # accepts new players
-            client_connector.start()
-            send_broadcast(clients)
-            client_connector.join()
-            time.sleep(1)  # waits 10 seconds after assign 2nd user
-            start_game(clients)  # play the game
-            closeSockets(clients)
-            print("Game over, sending out offer requests...")
+            try:
+                clients = list()  # client list
+                client_connector = Thread(target=connect_clients, args=(clients, sock,))  # accepts new players
+                client_connector.start()
+                send_broadcast(clients)
+                client_connector.join()
+                time.sleep(1)  # waits 10 seconds after assign 2nd user
+                start_game(clients)  # play the game
+                closeSockets(clients)
+                print("Game over, sending out offer requests...")
+            except Exception as e:
+                print("-----------------")
+                print(e)
+                print("-----------------")
+                break
     print("done")
 
 if __name__ == "__main__":
